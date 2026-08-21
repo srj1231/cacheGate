74a5e8e7542ca370ccee36edf010c0e66dba3dc1
srj1231 <saumyasrjjha@gmail.com>
2026-08-21 19:00:46 +0530

    project set up

---
+++ src/main/java/com/saumya/cachegate/CachegateApplication.java
@@ -0,0 +1,13 @@
+package com.saumya.cachegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachegateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachegateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saumya/cachegate/CachegateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saumya.cachegate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachegateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
